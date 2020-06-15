7e096f75022b8e396b8fd7eb1b89a3d27999ce4a
Nekololizu <Nekololizu@gmail.com>
2020-06-15 17:30:23 +0200

    Jungle mimic banner WIP

---
+++ Items/Banners/JungleMimicBanner.cs
@@ -0,0 +1,26 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NekoTweakMod.Items.Banners
+{
+    public class JungleMimicBanner : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.width = 10;
+            item.height = 24;
+            item.maxStack = 99;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.consumable = true;
+            item.rare = ItemRarityID.Blue;
+            item.value = Item.buyPrice(0, 0, 10, 0); // 10 silver
+            //item.createTile = TileType<MonsterBanner>();
+            item.placeStyle = 1;
+        }
+    }
+}
---
+++ NPCs/JungleMimic.cs
@@ -0,0 +1,20 @@
+using Terraria.ID;
+using System;
+using Terraria;
+using Terraria.ModLoader;
+using NekoTweakMod.Items.Banners;
+
+namespace NekoTweakMod.NPCs
+{
+    public class JungleMimic : GlobalNPC
+    {
+        public override void SetDefaults(NPC npc)
+        {
+           // if (NPCID.BigMimicJungle)
+            {
+                // banner = npc.type;
+                //  bannerItem = ItemType<Items.Banners.JungleMimicBanner>();
+            }
+        }
+    }
+}
